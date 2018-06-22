d8c9dc8f30d7296b19e8c2e6bf735019ae4a0b8a
Nikhil <agrawalnikhil2712@gmail.com>
2018-06-22 19:05:31 +0530

    Inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `      
+  <!--The content below is only a placeholder and can be replaced.-->
+  <div style="text-align:center">
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+    <input type="text" name="firstname" value="John">
+  </div>
+      
+   `,
+  styles: [`input{color:blue }`]
 })
 export class AppComponent {
   title = 'app';
